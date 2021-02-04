fdf93cc1a40b2329d2b385e4bc4e54324250bf72
Roma.Ost <r.ostrovskiy@digital-sector.ru>
2021-02-04 17:17:43 +0300

    main - добавил правки в докер, настроил mysql и PMA в docker

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=mysql;dbname=' . getenv('MYSQL_DATABASE'),
+    'username' => getenv('MYSQL_USER'),
+    'password' => getenv('MYSQL_PASSWORD'),
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
