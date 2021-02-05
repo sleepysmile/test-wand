62e6b14daed7e382189d665693a861b3b504cb38
Roma.Ost <r.ostrovskiy@digital-sector.ru>
2021-02-05 10:10:06 +0300

    test-wand - Добавил миграцию создания публикаций

--- migrations/m210205_070127_create_publication_table.php
+++ migrations/m210205_070127_create_publication_table.php
@@ -14,6 +14,10 @@ class m210205_070127_create_publication_table extends Migration
     {
         $this->createTable('{{%publication}}', [
             'id' => $this->primaryKey(),
+            'title' => $this->string()->comment('Заголовок публикации'),
+            'text' => $this->string()->comment('Тектс публикации'),
+            'status' => $this->tinyInteger()->comment('Статус публикации'),
+            'updated_at' => $this->timestamp()->comment('Дата изменения')
         ]);
     }
 
@@ -24,4 +28,5 @@ class m210205_070127_create_publication_table extends Migration
     {
         $this->dropTable('{{%publication}}');
     }
+
 }
