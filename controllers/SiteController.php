9b9ab3f07b8ced369c8d7a9cf24a636ce41dee68
Roma.Ost <ostdevops@gmail.com>
2021-02-06 17:24:15 +0300

    mani - controller

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,13 +2,9 @@
 
 namespace app\controllers;
 
-use Yii;
 use yii\filters\AccessControl;
-use yii\web\Controller;
-use yii\web\Response;
 use yii\filters\VerbFilter;
-use app\models\LoginForm;
-use app\models\ContactForm;
+use yii\web\Controller;
 
 class SiteController extends Controller
 {
