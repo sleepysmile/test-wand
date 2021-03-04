01691b1ef366982bf39175b11cd8c1e7457b5430
Ostrovskiy Roman <ostdevops@gmail.com>
2021-03-04 12:03:52 +0300

    main - поправил index файл, добавил новый пункт конфигурации базы данных

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=mysql;dbname=' . getenv('MYSQL_DATABASE'),
+    'dsn' => 'mysql:host=' . getenv('MYSQL_HOST') . ';dbname=' . getenv('MYSQL_DATABASE'),
     'username' => getenv('MYSQL_USER'),
     'password' => getenv('MYSQL_PASSWORD'),
     'charset' => 'utf8',
